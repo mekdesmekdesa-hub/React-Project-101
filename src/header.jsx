7d7cbde81fd73534e194f41877c75c9e9d08e2f3
mekdesmekdesa.hub <mekdesmekdesa@gmail.com>
2026-07-02 11:58:19 +0300

    setup for business card

--- src/header.jsx
+++ src/header.jsx
@@ -1,10 +1,7 @@
  function Header() {
     return (
         <header className="header-container" >
-             <div className="navbar-container">
-    <img src="src/assets/react.svg" alt="React logo" className='nav-logo'/>
-    <h1 className="nav-text">ReactFacts</h1>
-    </div>
+             
         </header>
     )
 }
