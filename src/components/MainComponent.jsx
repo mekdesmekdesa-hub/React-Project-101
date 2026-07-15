a5d009e92ec475c339ebf2b48f20bb552f64ba4d
mekdesmekdesa.hub <mekdesmekdesa@gmail.com>
2026-07-15 15:53:48 +0300

    improved header

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,7 +1,33 @@
+import { Phone, Mail, MapPin } from "lucide-react";
 function MainComponent() {
     return (
-        <main>
+        <main className="main-component">
+            <div className="name-container">
+                <h2>Mekdes M.</h2>
+                <div className="title-container">
+                    <h3>Frontend Developer</h3>
+                    <div className="line"></div>
+                </div>
+                
+            </div>
             
+            <div className="contact-container">
+                <ul>
+                    <li >
+                       <Phone className="phone-icon"/>
+                        <span>+251 93 152 1741</span>
+                    </li>
+                    <li>
+                        <Mail className="mail-icon" />
+                        <span>mekdesmekdesa@gmail.com</span>
+                    </li>   
+                    <li>
+                        <MapPin className="map-pin-icon" />
+                        <span>Addis Ababa, Ethiopia</span>
+                    </li>
+                </ul>
+            </div>
+           
         </main>
         
     )
--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,18 @@
  function Header() {
     return (
         <header className="header-container" >
-             
+             <div className="company-logo">
+                <img src="src/assets/Alem.png" alt="company-logo" className="company-logo-image"/>
+             </div>
+             <div className="company-name">
+                <h1>Alem Tech Solutions</h1>
+             </div>
+             <div className="company-slogan">
+                From ideas to impact
+             </div>
+             <div className="qr-code    ">
+                  <img src="src/assets/qrcode.png" alt="qr-code" className="qr-code-image"/>
+             </div>
         </header>
     )
 }
